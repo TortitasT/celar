d48b50b3e5d43600734db0caa32a2821dcd5c51a
bipo38 <104152564+bipo38@users.noreply.github.com>
2022-10-10 16:08:41 +0200

    fix install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd /tmp/celar_install
 
 $URL="https://api.github.com/repos/bipo38/celar/releases/latest"
 
-wget $URL
+wget "${URL}"
 cp ./celar ~/.local/bin
 echo "Install done! Restart terminal to apply changes..."
 
